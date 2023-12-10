77bbf971f838dcccbfe1b6692b80b00be211aa0a
BrunoPBQ <brunobarbaran@icloud.com>
2023-12-09 19:05:25 -0500

    feat: Add SideBar component for displaying session data
    This commit introduces a new SideBar component that is responsible for
    displaying session data in a tabular format. The component utilizes the
    useAutoAnimate hook from the @formkit/auto-animate/react package and
    relies on the UseSession context for session information and time
    formatting.
    
    The SideBar component renders a table with columns for session rankings,
    time, ao5 (average of 5), and ao12 (average of 12). The session data is
    mapped and displayed dynamically, with each row representing an item
    from the session array.
    
    This addition enhances the functionality of the application by providing
    a visual representation of session metrics, allowing users to easily
    analyze and compare their performance. It improves user experience and
    facilitates data interpretation.

---
+++ src/Components/SideBar/SideBar.jsx
@@ -0,0 +1,39 @@
+import { useAutoAnimate } from '@formkit/auto-animate/react'
+import { UseSession } from '../../Context/SessionContext';
+
+const SideBar = () => {
+    const [parent] = useAutoAnimate()
+    const { session, FormatTime } = UseSession()
+    
+    return (
+        <div className="d-flex flex-column flex-shrink-0 py-3 px-2 bg-body-tertiary h-100 rounded" style={{ width: '280px' }}>
+            <div className="out-table">
+                <table className="table table-sm text-center" style={{ width: '100%' }} >
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Time</th>
+                            <th>ao5</th>
+                            <th>ao12</th>
+                        </tr>
+                    </thead>
+                    <tbody ref={parent}>
+                        {session && session.length > 0 &&
+                            session.map((item, index) => {
+                                return (
+                                    <tr key={item.id}>
+                                        <td>{index + 1}</td>
+                                        <td>{FormatTime(item.time)}</td>
+                                        <td>{FormatTime(item.ao5)}</td>
+                                        <td>{FormatTime(item.ao12)}</td>
+                                    </tr>
+                                )
+                            })}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default SideBar;
