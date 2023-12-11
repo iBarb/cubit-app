f572c5b683818d25ea85f0f9f55283f005b8eb43
BrunoPBQ <brunobarbaran@icloud.com>
2023-12-11 00:16:09 -0500

    Refactor Timer component to display updated ao5 and ao12 times
    This commit refactors the Timer component to display the updated ao5 and
    ao12 times. The 'ao5' and 'ao12' values are obtained from the
    'UseSession' hook and formatted using the 'FormatTime' function.

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -1,13 +1,14 @@
 
 import './Timer.css';
 import { UseTimer } from "../../Hooks/UseTimer";
+import { UseSession } from '../../Context/SessionContext';
 
 
 
 const Timer = () => {
   const { hours, minutes, seconds, hnds, getClassNameByCode, statusTimer } = UseTimer();
-
-
+  const { ao5, ao12, FormatTime } = UseSession()
+  
   return (
     <div className="bg-body-tertiary d-flex justify-content-center align-items-center w-100 h-100 text-center rounded">
       <div>
@@ -21,10 +22,10 @@ const Timer = () => {
         </div>
         <div id='avg'>
           <div>
-            <span>ao5: 0.00</span>
+            <span>ao5: {FormatTime(ao5[ao5.length - 1])}</span>
           </div>
           <div>
-            <span> ao12: 0.00</span>
+            <span> ao12: {FormatTime(ao12[ao12.length - 1])}</span>
           </div>
         </div>
       </div>
