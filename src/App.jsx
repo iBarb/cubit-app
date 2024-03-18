1d60b49fe2494ac898cc89e966ff554a454730b6
Barb <brunobarbaran@icloud.com>
2024-03-17 20:05:09 -0500

    add View Transitions change place session provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
 import SideBar from "./Components/SideBar/SideBar"
 import Timer from "./Components/Timer/Timer"
-import { SessionProvider } from "./Context/SessionContext"
+import { UseTimer } from "./Context/TimerContext"
 
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { flushSync } from "react-dom";
 
 function App() {
+  const { statusTimer } = UseTimer();
+  const [IsFocus, setIsFocus] = useState(false)
+
+  useEffect(() => {
+    document.startViewTransition(() =>{
+      flushSync(() => {
+        if (statusTimer === 2) {
+          setIsFocus(true)
+    
+        } else if (statusTimer === 0) {
+          setIsFocus(false)
+        }
+      })
+    })
+
+  }, [statusTimer])
+
 
   return (
     <>
-      <SessionProvider>
-        <div className="d-flex h-100 gap-2 bg-body p-2">
+      {!IsFocus ?
+        <div className="d-flex h-100 gap-2 bg-body p-2 ">
           <SideBar />
           <Timer />
         </div>
-      </SessionProvider>
+        :
+        <div className="d-flex h-100 gap-2 bg-body p-2 ">
+          <Timer />
+        </div>
+
+      }
     </>
   )
 }
