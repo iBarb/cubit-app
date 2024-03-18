70707dbb9f97f6a4c64ab3259b12e55c00a33fe3
Barb <brunobarbaran@icloud.com>
2024-03-17 20:05:54 -0500

    add sessionProvider & TimerProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SessionProvider } from './Context/SessionContext.jsx'
+import { TimerProvider } from './Context/TimerContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SessionProvider>
+      <TimerProvider>
+        <App />
+      </TimerProvider>
+    </SessionProvider>
   </React.StrictMode>,
 )
