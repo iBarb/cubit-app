d3ef6ac1c0a1d28aec2e6b84c968c72bf860e65f
Barb <brunobarbaran@icloud.com>
2024-03-17 20:08:56 -0500

    Create new stats component

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,39 +1,11 @@
-import { useAutoAnimate } from '@formkit/auto-animate/react'
-import { UseSession } from '../../Context/SessionContext';
+import Stats from '../Stats/Stats';
 
 const SideBar = () => {
-    const [parent] = useAutoAnimate()
-    const { session, FormatTime } = UseSession()
 
     return (
-        <div className="d-flex flex-column flex-shrink-0 py-3 px-2 bg-body-tertiary h-100 rounded" style={{ width: '280px' }}>
-            <div className="out-table">
-                <table className="table table-sm text-center" style={{ width: '100%' }} >
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Time</th>
-                            <th>ao5</th>
-                            <th>ao12</th>
-                        </tr>
-                    </thead>
-                    <tbody ref={parent}>
-                        {session && session.length > 0 &&
-                            session.slice().reverse().map((item, index ,array) => {
-                                const reverseIndex = array.length - index;
-                                return (
-                                    <tr key={item.id}>
-                                        <td>{reverseIndex}</td>
-                                        <td>{FormatTime(item.time)}</td>
-                                        <td>{FormatTime(item.ao5)}</td>
-                                        <td>{FormatTime(item.ao12)}</td>
-                                    </tr>
-                                )
-                            })}
-                    </tbody>
-                </table>
-            </div>
-        </div>
+        <>
+            <Stats/>
+        </>
     );
 }
 
